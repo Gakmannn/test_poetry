d5819b1243870bc56c2f337410f04cab6d7f5bfb
GakmanNN <gnn87@mail.ru>
2023-03-19 18:02:58 +0300

    temlates

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest, JsonResponse
   
 
 def index(request):
+    # langs = []
+    colors = {"red": "красный", "green": "зеленый", "blue":"синий"}
+    langs = ["Python", "JavaScript", "Java", "C#", "C++"]
+    data = {"header": "Hello Django",
+            "message": "Welcome to Python", "person": Person("Bob", 41), "body": "<h1>Hello World!</h1>", "n": 0, "langs": langs, "data": colors, "users": ["Tom", "Sam", "Bob", "Mike"]}
+    return render(request, "index.html", context=data)
+
+def complex(request):
+    header = "Данные пользователя"              # обычная переменная
+    langs = ["Python", "Java", "C#"]            # список
+    user = {"name": "Tom", "age": 23}          # словарь
+    address = ("Абрикосовая", 23, 45)           # кортеж
+
+    data = {"header": header, "langs": langs, "user": user, "address": address}
+    return render(request, "complex.html", context=data)
+
+def meta(request):
     return HttpResponse(f'''
       {request.META}
       <br>  
@@ -39,13 +58,116 @@ def index(request):
     ''')
 
 
-def about(request, name, age):
+def getUser(request):
+    age = request.GET.get("age", 0)
+    name = request.GET.get("name", "unindent")
+    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+
+def user(request, name='undefined', age=0):
     return HttpResponse(f"""
             <h2>О пользователе</h2>
             <p>Имя: {name}</p>
             <p>Возраст: {age}</p>
     """)
 
-def contact(request):
-    return HttpResponse("Контакты")
+def about(request):
+    return HttpResponse("About")
+
 # Create your views here.
+
+
+def products(request, id=-1):
+    if id==-1:
+        return HttpResponse(f"Список товаров")
+    else:
+        return HttpResponse(f"Товар {id}")
+
+
+def comments(request, id):
+    return HttpResponse(f"Комментарии о товаре {id}")
+
+
+def questions(request, id):
+    return HttpResponse(f"Вопросы о товаре {id}")
+
+def new(request):
+    return HttpResponse("Новые товары")
+
+
+def top(request):
+    return HttpResponse("Наиболее популярные товары")
+
+
+def contact(request):
+    return HttpResponseRedirect("/about")
+
+
+def details(request):
+    return HttpResponsePermanentRedirect("/")
+
+
+def notFound(request):
+    return HttpResponseNotFound("404. Not Found")
+
+
+def people(request, id):
+    people = ["Tom", "Bob", "Sam"]
+    # если пользователь найден, возвращаем его
+    if id in range(0, len(people)):
+        return HttpResponse(people[id])
+    # если нет, то возвращаем ошибку 404
+    else:
+        return HttpResponseNotFound("Not Found")
+
+
+def access(request, age):
+    # если возраст НЕ входит в диапазон 1-110, посылаем ошибку 400
+    if age not in range(1, 111):
+        return HttpResponseBadRequest("Некорректные данные")
+    # если возраст больше 17, то доступ разрешен
+    if (age > 17):
+        return HttpResponse("Доступ разрешен")
+    # если нет, то возвращаем ошибку 403
+    else:
+        return HttpResponseForbidden("Доступ заблокирован: недостаточно лет")
+
+
+def json(request):
+    return JsonResponse({"name": "Tom", "age": 38})
+
+
+def person(request):
+    bob = Person("Bob", 41)
+    return JsonResponse(bob, safe=False, encoder=PersonEncoder)
+
+def notaperson(request):
+    bob = [1,2,4]
+    return JsonResponse(bob, safe=False, encoder=PersonEncoder)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name    # имя человека
+        self.age = age      # возраст человека
+
+class PersonEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Person):
+            return obj.__dict__
+            return {"name": obj.name, "age": obj.age}
+        return super().default(obj)
+
+# установка куки
+def set(request):
+    # получаем из строки запроса имя пользователя
+    username = request.GET.get("username", "Undefined")
+    # создаем объект ответа
+    response = HttpResponse(f"Hello {username}")
+    # передаем его в куки
+    response.set_cookie("username", username)
+    return response
+
+# получение куки
+def get(request):
+    # получаем куки с ключом username
+    username = request.COOKIES["username"]
+    return HttpResponse(f"Hello {username}")
--- metanit/metanit/urls.py
+++ metanit/metanit/urls.py
@@ -14,14 +14,43 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path
-from django.urls import path
+from django.urls import re_path, path, include
 from hello import views
+
+product_patterns = [
+    path("", views.products),
+    path("new", views.new),
+    path("top", views.top),
+]
+products_patterns = [
+    path("", views.products),
+    path("comments", views.comments),
+    path("questions", views.questions),
+]
 
 urlpatterns = [
     path('', views.index),
-    re_path(r'^about/contact', views.contact),
-    re_path(r'^about', views.about, kwargs={"name": "Tom", "age": 38}),
-    re_path(r'^contact', views.contact),
+    path("meta", views.meta),
+    path("complex", views.complex),
+    path("get", views.get),
+    path("set", views.set),
+    path("json", views.json),
+    path("person", views.person),
+    path("notaperson", views.notaperson),
+    path("people/<int:id>", views.people),
+    path("access/<int:age>", views.access),
+    path("products", include(product_patterns)),
+    path("products/<int:id>/", include(products_patterns)),
+    path("getuser/", views.getUser),
+    re_path(r"^user/(?P<name>\D+)/(?P<age>\d+)", views.user),
+    re_path(r"^user/(?P<name>\D+)", views.user),
+    re_path(r"^user", views.user),
+    # path("user", views.user),
+    # path("user/<name>", views.user),
+    # path('user/<name>/<int:age>', views.user),
+    re_path(r'^about', views.about),
+    path("contact/", views.contact),
+    path("details/", views.details), 
     path('admin/', admin.site.urls),
+    re_path(r"\D+", views.notFound),
 ]
